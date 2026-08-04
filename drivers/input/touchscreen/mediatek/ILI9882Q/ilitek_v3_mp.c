240425396b3687f18633756e2cc735221a6a143b
Daniella :3 <danithesillyfem@gmail.com>
2026-08-04 06:50:54 +0000

    fix

--- drivers/input/touchscreen/mediatek/ILI9882Q/ilitek_v3_mp.c
+++ drivers/input/touchscreen/mediatek/ILI9882Q/ilitek_v3_mp.c
@@ -654,7 +654,7 @@ copy:
 
 static int parser_get_ini_phy_data(char *data, int fsize)
 {
-	int i, n = 0, ret = 0, banchmark_flag = 0, empty_section, nodetype_flag = 0;
+	int i, n = 0, ret = 0, empty_section;
 	int offset = 0, isEqualSign = 0, scount = 0;
 	char *ini_buf = NULL, *tmpSectionName = NULL;
 	char M_CFG_SSL = '[';
@@ -686,9 +686,7 @@ static int parser_get_ini_phy_data(char *data, int fsize)
 	memset(seq_item, 0, MP_TEST_ITEM * PARSER_MAX_KEY_NAME_LEN * sizeof(char));
 
 	while (true) {
-		banchmark_flag = 0;
 		empty_section = 0;
-		nodetype_flag = 0;
 		if (g_ini_items > PARSER_MAX_KEY_NUM) {
 			ILI_ERR("MAX_KEY_NUM: Out of length\n");
 			goto out;
@@ -732,8 +730,6 @@ static int parser_get_ini_phy_data(char *data, int fsize)
 
 				ini_buf[n - 1] = 0x00;
 				strncpy((char *)tmpSectionName, ini_buf + 1, (PARSER_MAX_CFG_BUF + 1) * sizeof(char));
-				banchmark_flag = 0;
-				nodetype_flag = 0;
 				strncpy(seq_item[scount], tmpSectionName, PARSER_MAX_KEY_NAME_LEN);
 				scount++;
 				ILI_DBG("Section Name: %s, Len: %d, offset = %d\n", seq_item[scount], n - 2, offset);
